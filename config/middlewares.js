16a6796ff4b5f6a245f801a4fa5967c708bf0b94
andriivam <andriivlasiuk2@gmail.com>
2022-11-29 20:07:09 +0100

    fix security poicy issue

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,19 +3,20 @@ module.exports = [
   {
     name: 'strapi::security',
     config: {
+      // contentSecurityPolicy: false,
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com','https://res.cloudinary.com', 'https://dl.airtable.com', "'self' data: blob: https://dl.airtable.com"],
           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  'strapi::security',
-  'strapi::cors',
+  // 'strapi::security',
+   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
